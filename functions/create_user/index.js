17518c9ac56495af6875e47f3e84b56cf11e7579
Shawn Baugh II <shawnbaughcreative@gmail.com>
2018-07-15 11:34:26 -0500

    create user lambda update

--- functions/create_user/index.js
+++ functions/create_user/index.js
@@ -2,21 +2,27 @@ const aws = require('aws-sdk');
 const bcrypt = require('bcryptjs');
 const db = new aws.DynamoDB.DocumentClient({ region: 'us-west-2' });
 
-const userId =
-  Math.random()
-    .toString(36)
-    .substring(2, 15) +
-  Math.random()
-    .toString(36)
-    .substring(2, 15);
-
 exports.handle = function(e, ctx, cb) {
+  let userId = null;
+  userId =
+    Math.random()
+      .toString(36)
+      .substring(2, 15) +
+    Math.random()
+      .toString(36)
+      .substring(2, 15);
+
+  let email = null;
+  if (/\S*\@\S*\.\S+/g.test(e.email)) {
+    email = e.email;
+  }
+
   let params = {
     Item: {
       id: userId,
       users: {
         username: e.username,
-        email: /\S*\@\S*\.\S+/g.test(e.email) ? e.email : false,
+        email: email,
         password: bcrypt.hashSync(e.password, bcrypt.genSaltSync(10)),
         questions: e.questions,
         createdAt: Date.now()
@@ -24,13 +30,12 @@ exports.handle = function(e, ctx, cb) {
     },
     TableName: 'questions'
   };
+
   db.put(params, (err, data) => {
-    if (err || params.Item.user.email === false) {
+    if (err || params.Item.users.email === null) {
       cb(err, null);
     } else {
       cb(null, data);
     }
   });
-
-  userId = null;
 };
