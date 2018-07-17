b1660257e7e47f49ecb8138378d0e13fa121907d
Shawn Baugh II <shawnbaughcreative@gmail.com>
2018-07-17 00:06:38 -0500

    cognito setup fro create user lambda.

--- functions/create_user/index.js
+++ functions/create_user/index.js
@@ -1,26 +1,49 @@
+import { UserPoolId, ClientId } from './keys';
+import {
+  CognitoUserAttribute,
+  CognitoUserPool,
+  CognitoUser
+} from 'amazon-cognito-identity-js';
 const aws = require('aws-sdk');
 const bcrypt = require('bcryptjs');
 const db = new aws.DynamoDB.DocumentClient({ region: 'us-west-2' });
+const poolData = {
+  UserPoolId: UserPoolId,
+  ClientId: '4pe2usejqcdmhi0a25jp4b5sh3'
+};
+const userPool = new CognitoUserPool(poolData);
 
 exports.handle = function(e, ctx, cb) {
-  let userId = null;
-  userId =
-    Math.random()
-      .toString(36)
-      .substring(2, 15) +
-    Math.random()
-      .toString(36)
-      .substring(2, 15);
+  let attributeList = [];
+  let dataEmail = {
+    Name: 'email',
+    Value: 'anemail@email.com'
+  };
+  let attributeEmail = new CognitoUserAttribute(dataEmail);
 
+  attributeList.push(attributeEmail);
+  let userId = null;
+  userPool.signUp(
+    'username',
+    'password',
+    attributeList,
+    null,
+    (err, result) => {
+      if (err) {
+        return err.message;
+      } else {
+        CognitoUser = result.user;
+        console.log(`UserName is ${CognitoUser.getUsername()}`);
+      }
+    }
+  );
+  let date = new Date();
   let params = {
     Item: {
       id: userId,
       users: {
-        username: e.username,
-        email: /\S*\@\S*\.\S+/g.test(e.email) ? e.email : null,
-        password: bcrypt.hashSync(e.password, bcrypt.genSaltSync(10)),
         questions: e.questions,
-        createdAt: Date.now()
+        createdAt: date
       }
     },
     TableName: 'questions'
